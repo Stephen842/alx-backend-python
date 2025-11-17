327545aacae399b2151025f3442d4d91116ef7bb
stephen842 <ugotachisomstephen@gmail.com>
2025-11-17 16:19:26 +0100

    hotfix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,7 +7,35 @@ from parameterized import parameterized
 from unittest.mock import patch
 
 from client import GithubOrgClient
-from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
+
+#Import from fixtures.py the following to resolve import error
+org_payload = {
+    "login": "google",
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
+
+repos_payload = [
+    {
+        "id": 1,
+        "name": "repo1",
+        "license": {"key": "apache-2.0"}
+    },
+    {
+        "id": 2,
+        "name": "repo2",
+        "license": {"key": "mit"}
+    },
+    {
+        "id": 3,
+        "name": "repo3",
+        "license_key": {"key": "apache-2.0"}
+    }
+]
+
+expected_repos = ["repo1", "repo2", "repo3"]
+apache2_repos = ["repo1", "repo3"]
+
 
 
 class TestGithubOrgClient(unittest.TestCase):
