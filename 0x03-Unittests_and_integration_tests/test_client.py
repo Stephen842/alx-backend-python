38b0a07021e45c1d8be360f0ee84f5ea6083d4ac
stephen842 <ugotachisomstephen@gmail.com>
2025-11-17 14:36:42 +0100

    hotfix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,6 @@
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch
-from .utils import get_json
 
 from client import GithubOrgClient
 
@@ -19,8 +18,8 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns the expected result"""
-        expected_payload = {"login": org_name}
-        mock_get_json.return_value = expected_payload
+        expected = {"login": org_name}
+        mock_get_json.return_value = expected
 
         client = GithubOrgClient(org_name)
         result = client.org
@@ -28,13 +27,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
-        self.assertEqual(result, expected_payload)
+        self.assertEqual(result, expected)
 
     # Task 5
     def test_public_repos_url(self):
         """Test that _public_repos_url returns correct URL from org"""
         # Mock payload for org property
-        mock_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
 
         client = GithubOrgClient("google")
 
@@ -73,3 +74,4 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, ["repo1", "repo2", "repo3"])
             mock_get_json.assert_called_once_with(fake_url)
             mock_url.assert_called_once()
+            
