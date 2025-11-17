7660519515b2affdb1d74e043c012e66614ac595
stephen842 <ugotachisomstephen@gmail.com>
2025-11-17 16:27:31 +0100

    hotfix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -138,12 +138,12 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
     def test_public_repos(self):
         """Test public_repos returns expected repo names"""
-        client = GithubOrgClient("google")
+        client = GithubOrgClient(self.org_payload["login"])
         self.assertEqual(client.public_repos(), self.expected_repos)
 
     def test_public_repos_with_license(self):
         """Test public_repos filtered by license 'apache-2.0'"""
-        client = GithubOrgClient("google")
+        client = GithubOrgClient(self.org_payload["login"])
         self.assertEqual(
             client.public_repos(license_key="apache-2.0"),
             self.apache2_repos
