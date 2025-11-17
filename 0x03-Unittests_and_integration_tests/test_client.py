961a395527024f1b1a6a330820c3cdd08c87c3a5
stephen842 <ugotachisomstephen@gmail.com>
2025-11-17 15:24:32 +0100

    hotfix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,7 +7,7 @@ from parameterized import parameterized
 from unittest.mock import patch
 
 from client import GithubOrgClient
-from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
+from .fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
 
 class TestGithubOrgClient(unittest.TestCase):
