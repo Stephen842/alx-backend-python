d412298c0b54d1171e97deb750bcf202b08959d0
stephen842 <ugotachisomstephen@gmail.com>
2025-11-20 21:22:17 +0100

    Added a Validation and CharField across serialized models

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -6,12 +6,16 @@ from .models import User, Conversation, Message
 # User Serializer
 #----------------------------------
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'password']
+        fields = ['user_id', 'first_name', 'last_name', 'full_name', 'email', 'phone_number', 'role', 'password']
         extra_kwargs = {
             'password': {'write_only': True},
         }
+
+    def get_full_name(self, obj):
+        return f'{obj.first_name} {obj.last_name}'
 
 
 # ---------------------------------
@@ -19,11 +23,16 @@ class UserSerializer(serializers.ModelSerializer):
 #----------------------------------
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_preview = serializers.CharField(source='message_body', read_only=True)
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
         read_only_fields = ['sent_at']
 
+    def validate_message_body(self, value):
+        if not value.strip():
+            raise serializers.ValidationError('Message body cannot be empty')
+        return value
 
 
 # ---------------------------------
