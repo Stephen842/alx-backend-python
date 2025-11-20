3480468cf7e182efb8ca1972a85a8bc24dc10eab
stephen842 <ugotachisomstephen@gmail.com>
2025-11-21 00:06:19 +0100

    hotfix

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,15 +2,15 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
-from .views import MessageViewset, ConversationViewset
+from .views import MessageViewSet, ConversationViewSet
 
 ### DRF Base Router
 router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewset, basename='conversations')
+router.register(r'conversations', ConversationViewSet, basename='conversations')
 
 # DRF Nested Router for messages under conversation
 conversation_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
-conversation_router.register(r'messages', MessageViewset, basename='conversation-message')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-message')
 
 urlpatterns = [
     path('', include(router.urls)),
