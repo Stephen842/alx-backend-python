6f7963bab6f49a900c39bef6a5cb7d7a67c533c1
stephen842 <ugotachisomstephen@gmail.com>
2025-11-20 22:42:10 +0100

    Built the Api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_frame import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import User, Conversation, Message
+from .serializers import MessageSerializer, ConversationSerializer
 
-# Create your views here.
+
+# ---------------------------------
+# Conversation ViewSet
+#---------------------------------
+class ConversationViewset(viewsets.ModelViewSet):
+    '''
+    API endpoint for conversations:
+    - List all conversations
+    - Retrieve a conversation
+    - Create a new conversation
+    '''
+
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    authentication_class = [TokenAuthentication, SessionAuthentication]
+    permission_class = [IsAuthenticated]
+
+
+# ---------------------------------
+# Message ViewSet
+#---------------------------------
+class MessageViewset(viewsets.ModelViewSet):
+    '''
+    API endpoint for messages:
+    - List messages
+    - Retrieve a message
+    - Create a new message
+    '''
+
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    authentication_class = [TokenAuthentication, SessionAuthentication]
+    permission_class = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        '''
+        Automatically set the sender to be the current signed in user
+        '''
+        serializer.save(sender=self.request.user)
