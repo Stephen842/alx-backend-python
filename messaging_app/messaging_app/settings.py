b607f6663d7687f2ebf1fc0d63c7e32cb829f092
stephen842 <ugotachisomstephen@gmail.com>
2025-11-20 20:04:50 +0100

    Add DRF authentication and permission settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -85,7 +85,11 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 # Password validation
