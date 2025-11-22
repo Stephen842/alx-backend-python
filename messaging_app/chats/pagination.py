78764b2f4dfac3ccfb7e7995ef942d399003c007
stephen842 <ugotachisomstephen@gmail.com>
2025-11-22 11:38:36 +0100

    hotfix

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -5,3 +5,6 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return super().get_paginated_response(data)
