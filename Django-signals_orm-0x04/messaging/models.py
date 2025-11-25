335686c614096f5bfe36596546f44a5e480ff4db
stephen842 <ugotachisomstephen@gmail.com>
2025-11-25 17:12:50 +0100

    hotfix

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
-    reciever = models.ForeignKey(User, related_name='recieved_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='recieved_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -15,7 +15,7 @@ class Message(models.Model):
 
 class Notification(models.Models):
     user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
-    message = models.ForeignKey(Message, related_name='notification', on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
